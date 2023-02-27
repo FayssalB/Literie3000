9b1a03a2002b6daa8ae8b28845cf95cc0710fc7b
Fayssal Belghazi <faysstoface@hotmail.fr>
2023-02-27 10:55:25 +0100

    Recuperation et affichage des données : OK

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+$dsn = "mysql:host=localhost;dbname=literie3000";
+$db = new PDO($dsn, "root", "");
+
+$query = $db->query("SELECT * from matelas");
+$matelas = $query->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($_POST);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +19,40 @@
     <link rel="stylesheet" href="css/main.css">
     <title>Literie</title>
 </head>
+
 <body>
-    <h1>Dodo3000</h1>
+    <header>
+        <div>Literie3000</div>
+        <div>
+            <ul>
+                <li><a href="">Nos produits</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="container">
+        <!-- <pre><?= var_dump($matelas) ?></pre>  -->
+        <h1>Nos produits</h1>
+        <?php
+        if ($matelas) {
+            foreach ($matelas as $matela) {
+        ?>
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src="<?= $matela["image"] ?>" alt="">
+                    </div>
+                    <h3><?= $matela["fabricant"] ?></h3>
+                    <p><?= $matela["modele"] ?></p>
+                    <p><?= $matela["prix"] ?>€</p>
+                   <a href="details.php?id=<?=$matela["id"]?>">Détails</a>
+                   <a href="delete.php">Delete</a>
+                </div>
+        <?php
+            }
+        }
+        ?>
+
+
+    </div>
 </body>
+
 </html>
